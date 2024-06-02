52e61ff1e7250249ee81cddc5ca006544fe6a296
Pablo Fajardo <pfajardodiaz17@gmail.com>
2024-06-02 13:01:30 -0500

    Basic Project

---
+++ controllers/products.js
@@ -0,0 +1,19 @@
+exports.getProducts = (req, res, next) => {
+
+}
+
+exports.getProductById = (req, res, next) => {
+
+}
+
+exports.createProduct = (req, res, next) => {
+
+}
+
+exports.updateProduct = (req, res, next) => {
+
+}
+
+exports.deleteProduct = (req, res, next) => {
+
+}
